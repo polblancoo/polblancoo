f491081046cd13fd5fcf1c887ca2e44025d7418f
Polblancoo <polblancoo@gmail.com>
2022-04-02 09:43:13 -0300

    otra prueba

--- guessing_game/target/debug/deps/guessing_game-666b46bfad880916.d
+++ guessing_game/target/debug/deps/guessing_game-666b46bfad880916.d
@@ -1,5 +1,5 @@
-/home/pablo/Rust-App/guessing_game/target/debug/deps/guessing_game-666b46bfad880916.rmeta: src/main.rs
+/home/pablo/Rust-Proyectos/guessing_game/target/debug/deps/guessing_game-666b46bfad880916.rmeta: src/main.rs
 
-/home/pablo/Rust-App/guessing_game/target/debug/deps/guessing_game-666b46bfad880916.d: src/main.rs
+/home/pablo/Rust-Proyectos/guessing_game/target/debug/deps/guessing_game-666b46bfad880916.d: src/main.rs
 
 src/main.rs:
---
+++ guessing_game/target/rls/debug/deps/guessing_game-666b46bfad880916.d
@@ -0,0 +1,5 @@
+/home/pablo/Rust-Proyectos/guessing_game/target/rls/debug/deps/guessing_game-666b46bfad880916.rmeta: src/main.rs
+
+/home/pablo/Rust-Proyectos/guessing_game/target/rls/debug/deps/guessing_game-666b46bfad880916.d: src/main.rs
+
+src/main.rs:
